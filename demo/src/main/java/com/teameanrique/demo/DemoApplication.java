b714eedaea94fc5c98b5418c01dedb266a776bdb
Kentboyd <kmboyd18@gmail.com>
2023-09-09 14:06:26 -0500

    gradle build

--- demo/src/main/java/com/teameanrique/demo/DemoApplication.java
+++ demo/src/main/java/com/teameanrique/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.teameanrique.demo;
+package main.java.com.teameanrique.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
